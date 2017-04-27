1b25fd4fbb9125f3ac5f353f279f2fb9a7d567eb
Jackzzz126 <Jackzzz@126.com>
2017-04-27 17:09:26 +0800

    Update util & add test

---
+++ testUtil.py
@@ -0,0 +1,27 @@
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import unittest
+import util
+
+class UtilTest(unittest.TestCase):
+	def setUp(self):
+		pass
+
+	def tearDown(self):
+		pass
+
+	def test_pos_index_2_str(self):
+		self.assertEqual(util.pos_index_2_str(0, 1 - 1), 'A1', 'pos_index_2_str fail')
+		self.assertEqual(util.pos_index_2_str(9, 26 - 1), 'Z10', 'pos_index_2_str fail')
+		self.assertEqual(util.pos_index_2_str(999, 26 ** 1 * 1 + 26 - 1),\
+				'AZ1000', 'pos_index_2_str fail')
+		self.assertEqual(util.pos_index_2_str(0, 26 ** 1 * 2 + 1- 1), 'BA1', 'pos_index_2_str fail')
+		self.assertEqual(util.pos_index_2_str(0, 26 ** 1 * 26 + 2 - 1), 'ZB1', 'pos_index_2_str fail')
+		self.assertEqual(util.pos_index_2_str(0, 26 ** 2 * 1 + 26 ** 1 * 26 + 2 - 1),\
+				'AZB1', 'pos_index_2_str fail')
+		self.assertEqual(util.pos_index_2_str(0, 26 ** 2 * 3 + 26 ** 1 * 26 + 2 - 1),\
+				'CZB1', 'pos_index_2_str fail')
+
+if __name__ == '__main__':
+	unittest.main()
--- util.py
+++ util.py
@@ -9,3 +9,19 @@ def read(file_name):
 			break
 		print sheet.title
 	#print global_data.excel_path + file_name
+
+def pos_index_2_str(row_index, col_index):
+	col = col_index + 1
+	colStr = ""
+
+	while True:
+		if col % 26 > 0:
+			colStr = chr(ord("A") + col % 26 - 1) + colStr
+			col = col / 26
+		else:#col % 26 = 0
+			if col == 0:
+				break
+			else:
+				colStr = chr(ord("A") + 26 - 1) + colStr
+				col = col / 26 - 1
+	return colStr + str(row_index + 1)
