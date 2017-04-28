6f6168b2e52a4584617e1a2ad5c446e99d8a5fc8
Jackzzz126 <Jackzzz@126.com>
2017-04-28 18:55:47 +0800

    Update

--- export.py
+++ export.py
@@ -1,19 +1,10 @@
 import os
 import json
 import openpyxl
+import traceback
 
 import global_data
 import util
-
-class DataType(object):
-	data_type = ""#int, string
-	value_min = 1#int
-	value_max = 0#int
-	min_len = 0#string
-	max_len = 0#string
-	not_null = True
-	is_array = False
-	id_type = "id"#id, combineid
 
 def run():
 	items = os.listdir(global_data.excel_path)
@@ -47,15 +38,105 @@ def read_sheet_type(file_name, sheet):
 		if keyField in global_data.gTypes[keyFile][keySheet].keys():
 			util.add_error(keyFile, keySheet, 0, col, "Duplicate data field")
 
-		global_data.gTypes[keyFile][keySheet][keyField] = read_type(sheet, col)
+		global_data.gTypes[keyFile][keySheet][keyField] = read_type(keyFile, keySheet, sheet, col)
 
 		col += 1
 
-def read_type(sheet, col):
-	typeDesc = json.loads(sheet[util.pos_index_2_str(3, col)].value)
-	for k, v in typeDesc.items():
-		print k
-		print v
+class DataType(object):
+	data_type = ""
+	min_value = 1
+	max_value = 0
+	min_len = 0
+	max_len = 0
+	not_null = True
+	id_type = "id"
+	is_array = False
+	allowd_values = []
 
-	return None
+def read_type(keyFile, keySheet, sheet, col):
+	try:
+		type_desc = json.loads(sheet[util.pos_index_2_str(3, col)].value)
+		data_type = DataType()
+		if type_desc["dataType"] == "int":
+			if not isinstance(type_desc["minValue"], int) or\
+				not isinstance(type_desc["maxValue"], int) or\
+				type_desc["minValue"] > type_desc["maxValue"] or\
+				not isinstance(type_desc["notNull"], bool) or\
+				not isinstance(type_desc["isArray"], bool) or\
+				not isinstance(type_desc["allowedValues"], list):
+				raise Exception("")
 
+			for i in type_desc["allowedValues"]:
+				if not isinstance(type_desc["allowedValues"][i], int):
+					raise Exception("")
+
+			if type_desc["idType"] != None and \
+				type_desc["idType"] != "id" and \
+				type_desc["idType"] != "combinedid":
+				raise Exception("")
+
+			data_type.data_type = type_desc["dataType"]
+			data_type.min_value = type_desc["minValue"]
+			data_type.max_value = type_desc["maxValue"]
+			data_type.not_null = type_desc["notNull"]
+			data_type.id_type = type_desc["idType"]
+			data_type.is_array = type_desc["isArray"]
+			data_type.allowd_values = type_desc["allowedValues"]
+		elif type_desc["dataType"] == "float":
+			if not isinstance(type_desc["minValue"], float) or\
+				not isinstance(type_desc["maxValue"], float) or\
+				type_desc["minValue"] > type_desc["maxValue"] or\
+				not isinstance(type_desc["notNull"], bool) or\
+				not isinstance(type_desc["isArray"], bool) or\
+				not isinstance(type_desc["allowedValues"], list):
+				raise Exception("")
+
+			for i in type_desc["allowedValues"]:
+				if not isinstance(type_desc["allowedValues"][i], float):
+					raise Exception("")
+
+			if type_desc["idType"] != None and \
+				type_desc["idType"] != "id" and \
+				type_desc["idType"] != "combinedid":
+				raise Exception("")
+
+			data_type.data_type = type_desc["dataType"]
+			data_type.min_value = type_desc["minValue"]
+			data_type.max_value = type_desc["maxValue"]
+			data_type.not_null = type_desc["notNull"]
+			data_type.id_type = type_desc["idType"]
+			data_type.is_array = type_desc["isArray"]
+			data_type.allowd_values = type_desc["allowedValues"]
+		elif type_desc["dataType"] == "string":
+			if not isinstance(type_desc["minLen"], int) or\
+				not isinstance(type_desc["maxLen"], int) or\
+				type_desc["minLen"] < 1 or\
+				type_desc["minLen"] > type_desc["maxLen"] or\
+				not isinstance(type_desc["notNull"], bool) or\
+				not isinstance(type_desc["isArray"], bool) or\
+				not isinstance(type_desc["allowedValues"], list):
+				raise Exception("")
+
+			for i in type_desc["allowedValues"]:
+				if not isinstance(type_desc["allowedValues"][i], str):
+					raise Exception("")
+
+			if type_desc["idType"] != None and \
+				type_desc["idType"] != "id" and \
+				type_desc["idType"] != "combinedid":
+				raise Exception("")
+
+			data_type.data_type = type_desc["dataType"]
+			data_type.min_len = type_desc["minLen"]
+			data_type.max_len = type_desc["maxLen"]
+			data_type.not_null = type_desc["notNull"]
+			data_type.id_type = type_desc["idType"]
+			data_type.is_array = type_desc["isArray"]
+			data_type.allowd_values = type_desc["allowedValues"]
+		else:
+			raise Exception("")
+
+	except Exception:
+		print traceback.format_exc()
+		util.add_error(keyFile, keySheet, 3, col, "Type describe error")
+		return None
